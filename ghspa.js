492e0e66571d85ef777e638a9a62969a8a1de151
soapyme <43958180+soapyme@users.noreply.github.com>
2018-10-08 20:44:34 +0800

    Update ghspa.js

--- ghspa.js
+++ ghspa.js
@@ -22,9 +22,7 @@
    /* redirect all 404 trafic to index.html */
    function redirect() {
      l.replace(l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') + '/?' +
-              (l.pathname ? 'p=' + l.pathname.replace(/&/g, '~and~').replace(repo, '') : '') +
-              (l.search ? '&q=' + l.search.slice(1).replace(/&/g, '~and~') : '') +
-              (l.hash))
+              (l.pathname ? 'ref=' + l.pathname : ''))
    }
 
    /* resolve 404 redirects into internal routes */
